8ea1ff6b5b41ffd49a78cd442b584185e37124ce
Raghav GC <110081656+raghav0011@users.noreply.github.com>
2022-12-12 13:55:23 +0545

    api call commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
       <header className="Navbar">
         <img className="logo" src="./images/TrollFace.png" />
         <h2 className="meme--title">Meme Generator</h2>
-        <h4 className="course--title">React Course - Project 3</h4>
+        <h4 className="course--title">React - Project 3</h4>
       </header>
     </>
   );
